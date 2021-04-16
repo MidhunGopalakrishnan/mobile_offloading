fc24009502e628fbd23a199f3b172f4e8111ea48
midhungopalakrishnan <midhun.gopalakrishnan@outlook.com>
2021-04-16 16:18:34 -0700

    Initial changes

--- app/src/main/java/com/amsy/mobileoffloading/MasterDiscovery.java
+++ app/src/main/java/com/amsy/mobileoffloading/MasterDiscovery.java
@@ -1,14 +1,24 @@
 package com.amsy.mobileoffloading;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MasterDiscovery extends AppCompatActivity {
+
+    private Button bDiscoveryComplete;
+    private RecyclerView rvConnectedDevices;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_master_discovery);
+
+        bDiscoveryComplete = findViewById(R.id.b_discovery_done);
+        rvConnectedDevices = findViewById(R.id.rv_connected_devices);
+
+
     }
 }
