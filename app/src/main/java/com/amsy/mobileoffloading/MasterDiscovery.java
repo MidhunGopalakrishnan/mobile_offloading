3b157258f05da97b00544493256781bc17bd7717
avoona <avoona@asu.edu>
2021-04-16 15:28:23 -0700

    Added Main Activity Permissions code added Added MasterDiscovery and ClientAdvertisement Activity

---
+++ app/src/main/java/com/amsy/mobileoffloading/MasterDiscovery.java
@@ -0,0 +1,14 @@
+package com.amsy.mobileoffloading;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MasterDiscovery extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_master_discovery);
+    }
+}
