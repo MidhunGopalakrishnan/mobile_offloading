bbfd323b0fd14f9278df4db7210f6c892bdbbf36
avoona <avoona@asu.edu>
2021-04-23 18:51:39 -0700

    Flush to file with needle

--- app/src/main/java/com/amsy/mobileoffloading/helper/FlushToFile.java
+++ app/src/main/java/com/amsy/mobileoffloading/helper/FlushToFile.java
@@ -6,21 +6,25 @@ import android.util.Log;
 import java.io.File;
 import java.io.IOException;
 
+import needle.Needle;
+
 public class FlushToFile {
     //TODO: Append is not needed since we are flushing the entire stream to file
-    public static void writeTextToFile(Context context, String filename, boolean append, String text) {
-        File path = context.getFilesDir();
-        File file = new File(path, filename);
-
-        try {
-            text = text + "\n";
-            java.io.FileWriter fileWriter = new java.io.FileWriter(file, append);
-            fileWriter.write(text);
-            fileWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-            Log.d("FLUSHTOFILE", "Writing to file failed.");
-        }
+    public static void writeTextToFile(Context context, String filename, boolean append, String str) {
+        Needle.onBackgroundThread().execute(() -> {
+            File path = context.getFilesDir();
+            File file = new File(path, filename);
+            String text = str;
+            try {
+                text = text + "\n";
+                java.io.FileWriter fileWriter = new java.io.FileWriter(file, append);
+                fileWriter.write(text);
+                fileWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.d("FLUSHTOFILE", "Writing to file failed.");
+            }
+        });
     }
 
     public static void writeMatrixToFile(Context context, String filename, int[][] matrix) {
